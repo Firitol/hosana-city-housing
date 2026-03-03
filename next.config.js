288991046a115e7b317661294c45b9801308ac73
Firitol <92720354+Firitol@users.noreply.github.com>
2026-03-04 01:18:13 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: true,
   images: {
     domains: ['public.vercel-storage.com'],
     remotePatterns: [
